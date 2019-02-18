5a70324c535c8dedb8a530f8de75222f806a3c09
jerager <jer@cs.amherst.edu>
2019-02-18 13:52:37 +0100

    Add files via upload

---
+++ lab1A.pl
@@ -0,0 +1,20 @@
+%Anyone passing their exams and winning the lottery is happy.
+%But anyone who studies or is lucky can pass all their exams. John did not
+%study but he is lucky. Anyone who is lucky wins the lottery. Is John happy?
+
+happy(X) :- pass(X,exams),win(X,lottery).
+pass(X,-) :- study(X).
+pass(X,Y) :- lucky(X).
+lucky(john).
+win(X,lottery) :- lucky(X).
+    
+% the following is needed because eclipse prolog objected to calling 
+% undefined predicates.
+
+study(someone_does).
+
+%
+% prolog query is ?- happy(john).
+%
+
+
---
+++ lab1B.pl
@@ -0,0 +1,10 @@
+%
+% All people that are solvent and are smart are happy. Those people that
+% can read are smart. John can read. If someone is wealthy
+% they are solvent. Happy people have exciting lives. John is wealthy.
+%
+% Is John happy?
+%
+%  Note: DO NOT USE read as the predicate for reading, it is a builtin and
+%  your program will not work correctly!  Use a different name.
+
